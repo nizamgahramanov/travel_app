3194ccebf37a30e31469fc592c7b1e503ca6e68a
Nizam <gahramanovnizam@gmail.com>
2022-10-13 14:25:19 +0400

    some

--- lib/screen/advanced_search_screen.dart
+++ lib/screen/advanced_search_screen.dart
@@ -5,6 +5,42 @@ class AdvancedSearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: [
+        SliverPersistentHeader(delegate: CustomSliverAppBarDelegate(expandedHeight:200), pinned: true,),
+        buildImages()
+      ],
+    );
   }
+  Widget buildImages()=>SliverGrid(delegate: SliverChildBuilderDelegate((context, index) => ImageWidget(index:index),childCount: 20), gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2))
 }
+
+class CustomSliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+  CustomSliverAppBarDelegate({ required this.expandedHeight});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    return Stack(fit: StackFit.expand, children: [buildAppBar(shrinkOffset)],);
+  }
+
+  Widget buildAppBar(double shrinkOffset) => AppBar(
+
+  );
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => expandedHeight;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => kToolbarHeight+30;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    throw UnimplementedError();
+  }
+  
+}
+
